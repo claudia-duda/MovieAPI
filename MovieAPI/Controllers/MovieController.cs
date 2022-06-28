465689298e090abbc3a41ffcd95dd6837d722c21
Claudia Ramos <claudiaeduarda_duda@hotmail.com>
2022-06-28 11:27:04 -0300

    Using http to all requests

--- MovieAPI/Controllers/MovieController.cs
+++ MovieAPI/Controllers/MovieController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MovieAPI.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MovieAPI.Controllers
 {
@@ -9,12 +10,31 @@ namespace MovieAPI.Controllers
     public class MovieController : ControllerBase
     {
         private static List<Movie> movies= new List<Movie>();
+        private static int id = 1;
 
         [HttpPost]
-        public void PostMovie(Movie movie)
+        public IActionResult PostMovie(Movie movie)
         {
+            movie.Id = id++;
             movies.Add(movie);
+            return CreatedAtAction(nameof(GetMovie), new { Id = movie.Id }, movie);
         }
-       
+        [HttpGet]
+        public IActionResult GetAllMovies()
+        {
+            return Ok(movies);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetMovie(int id)
+        {
+           Movie movie = movies.FirstOrDefault(movie => movie.Id == id);
+            if (movie != null)
+            {
+                return Ok(movie);
+            }
+            return NotFound();
+            
+        }
     }
 }
